747f1d7d79202612da965704c7969f988783fd7e
Saulius Anusas <saulius.anusas@gmail.com>
2023-11-06 19:51:54 +0200

    Priimtas Saruno pakeitimas

--- funk.py
+++ funk.py
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 def sum_digits(a: int, b: int) -> int:
+=======
+def sum_numbers(a: int, b: int) -> int:
+>>>>>>> c774921 (Fixed speling)
     return a + b
 
 def get_int():
     a = int(input("Enter a: "))
     b = int(input("Enter b: "))
     return a, b
+
